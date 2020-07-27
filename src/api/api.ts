a208e7d329665ccb48b704c179ba2e99c164cc18
Denys <gryshko_denys@gmail.com>
2020-07-27 22:41:04 +0300

    set api for categories

---
+++ src/api/api.ts
@@ -0,0 +1,17 @@
+export const getData = async <T>( type: string ): Promise<T[]> => {
+  const currentList = [];
+  let currentPage = 1;
+  let nextPage = 'exists';
+
+  if (type) {
+    while(nextPage) {
+      const response2 = await fetch(`http://swapi.dev/api/${type}/?page=${currentPage}`).then(respond => respond.json());
+      currentList.push(...response2.results)
+      currentPage++;
+      nextPage = response2.next;
+    }
+  }
+
+
+  return currentList;
+};
